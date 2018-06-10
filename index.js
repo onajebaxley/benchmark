bc3ae7e5b71063efb5a7aec546226bc12c9a58bc
onajebaxley <onaje.baxley@gmail.com>
2018-06-10 01:45:09 -0400

    Add docs.

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 const data = fs.readFileSync('./demographics.csv');
 
+
+///////////////////////
+//   Configuration   //
+///////////////////////
+
+// const URI = 'mongodb://mongo-0.mongo:27017,mongo-1.mongo:27017,mongo-2.mongo:27017?replicaSet=rs0';
+const URI = 'mongodb://mongo-0.mongo:27017';
+const TABLE_NAME = 'demographics';
+const TABLE_OPTIONS = { };
+const TARGET_RECORD_QUANTIY = 1000;
+
+
+/**
+ * Returns an array of objects generated from the given CSV data,
+ * where each objects keys are the lowercase headers on the first line
+ * of the CSV.
+ *
+ * @param {string} dataFile The data read from some csv
+ *
+ * @return {Array} Containing objects defined in the csv 
+ */
 function parseCsvData(dataFile) {
     let dataArr = bufferString.toString().split('\n');
     let headers = dataArr[0].toLowerCase().split(',');
@@ -31,52 +52,19 @@ function parseCsvData(dataFile) {
     return formattedObjects;
 }
 
-// const uri = 'mongodb://mongo-0.mongo:27017,mongo-1.mongo:27017,mongo-2.mongo:27017?replicaSet=rs0';
-const uri = 'mongodb://mongo-0.mongo:27017';
-
-console.log(`Attempting to connect to ${uri}...`);
-MongoClient.connect(uri, init);
-
-function init(err, clientConn) {
-    if (err) {
-        console.log(`ERROR connecting to ${uri}`);
-        console.log(err.message);
-        return;
-    }
-    console.log(`Connected.`);
-
-    const dbName = 'test';
-    const db = clientConn.db(dbName);
-
-    // check server config
-    const config = db.serverConfig;
-    console.log('db server config:');
-    console.log(config);
-
-    // Create a collection
-    const newTableName = 'demographics';
-    const newTableOptions = { };
-    console.log(`Creating arbitrary ${newTableName} collection...`);
-    db.createCollection(newTableName, newTableOptions).then((res) => {
-        console.log(`Result of creation: ${res}`);
-
-        // Run test
-        return testDb(db, newTableName);
-    }).then((res) => {
-        console.log(`Test completed. Time diff: ${res}`);
-
-        clientConn.close();
-        return;
-    }).catch((err) => {
-        console.log(`ERROR bencharmking ${newTableName}: ${err.message}`);
-        clientConn.close();
-    });
-}
-
-function testDb(collection) {
+/**
+ * Computes the time (in nanoseconds) to insert some target amount of
+ * objects into the given Collection.
+ *
+ * @param {Collection} collection The MongoClient.Collection receiving records
+ * @param {int} numRecords The number of records to insert
+ *
+ * @return {int} The time (in nanoseconds) to insert the target number of
+ *               records.
+ */
+function timeRecordInsertion(collection, numRecords) {
     return new Promise((resolve, reject) => {
         let sampleData = parseCsvData(data);
-        let numRecords = 1000;
         let hrTime = process.hrtime();
         let startTime = hrTime[0] * 1000000000 + hrTime[1]; // start in sE(-9)
 
@@ -107,6 +95,46 @@ function testDb(collection) {
     });
 }
 
+console.log(`Attempting to connect to ${URI}...`);
+MongoClient.connect(URI, init);
+
+/**
+ * Entrypoint: creates the desired table and performs benchmark.
+ */
+function init(err, clientConn) {
+    if (err) {
+        console.log(`ERROR connecting to ${URI}`);
+        console.log(err.message);
+        return;
+    }
+    console.log(`Connected.`);
+
+    const dbName = 'test';
+    const db = clientConn.db(dbName);
+
+    // check server config
+    const config = db.serverConfig;
+    console.log('db server config:');
+    console.log(config);
+
+    // Create a collection
+    console.log(`Creating arbitrary ${TABLE_NAME} collection...`);
+    db.createCollection(TABLE_NAME, TABLE_OPTIONS).then((res) => {
+        console.log(`Result of creation: ${res}`);
+
+        // Run test
+        return timeRecordInsertion(res, TARGET_RECORD_QUANTIY);
+    }).then((res) => {
+        console.log(`Test completed. Time diff: ${res}`);
+
+        clientConn.close();
+        return;
+    }).catch((err) => {
+        console.log(`ERROR bencharmking ${TABLE_NAME}: ${err.message}`);
+        clientConn.close();
+    });
+}
+
 module.exports.init = init;
-module.exports.testDb = testDb;
+module.exports.timeRecordInsertion = timeRecordInsertion;
 
